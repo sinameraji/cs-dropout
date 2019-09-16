05b0743bbc1285b464d5701dac5d091d824286d3
Sina Meraji <sina.meraji@gmail.com>
2019-09-16 16:18:15 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="imposter">
+          <h1>The real reason you wanna drop out of Computer Science</h1>
+          <p>there's a good chance it may be the system, more than it's you, or the field, causing that feeling.</p>
+        </div>
       </header>
+      <h4>Here's a checklist for you to see what might be going wrong:</h4>
+      <div className="one App-header">
+      <div className="imposter">
+      <h1>Imposter Syndrome</h1>
+          <h3>noun</h3> 
+          <p>anxiety or self-doubt that results from persistently undervaluing one’s competence and active role in achieving success, while falsely attributing one's accomplishments to luck or other external forces.</p>
+        </div>
+      </div>
+      <div className="two App-header"></div>
+      <div className="three App-header"></div>
     </div>
   );
 }
---
+++ src/Card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const card = (props) => {
+    return(
+        <div className="imposter">
+          <h1>{props.h1}</h1>
+          <h3>noun</h3> 
+          <p>anxiety or self-doubt that results from persistently undervaluing one’s competence and active role in achieving success, while falsely attributing one's accomplishments to luck or other external forces.</p>
+        </div>
+    )
+}
+
+export default card
